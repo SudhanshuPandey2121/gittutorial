324e11964cdd6b2983ab1e4b98876cda24f00dca
SudhanshuPandey2121 <stg271004@gmail.com>
2025-08-06 14:19:03 +0530

    replaced sum with dum

---
+++ prod.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int prod=1;
+    for(int i=1;i<5;i++) prod*=i;
+    return prod;
+}
---
+++ sum.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int dum=0;
+    for(int i=1;i<5;i++) dum+=i;
+    return dum;
+}
